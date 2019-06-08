a079c1b33408db4a45b3cb87b29c1af86b5b389c
Sean McPherson <seanmcp25@gmail.com>
2019-06-08 15:20:34 -0400

    Working prototype

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello world')
+this.initState = function(value) {
+    setAppState(value)
+}
+
+function getAppState() {
+    return JSON.parse(window.localStorage.getItem('state'))
+}
+
+function setAppState(state) {
+    window.localStorage.setItem('state', JSON.stringify(state))
+    delete this.state
+    this.state = state
+    Object.freeze(this.state)
+}
+
+this.setState = function(update, callback) {
+    const prevState = getAppState()
+    const nextState = {
+        ...state,
+        ...(typeof update === 'function' ? update(prevState) : update)
+    }
+    setAppState(nextState)
+    if (callback) callback(nextState)
+}
